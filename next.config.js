1f77592d3c704e7e371248399cdef8ee421af36d
Shishir Neupane <shishirs520@gmail.com>
2023-02-10 10:04:08 +0545

    update .gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+		allowMiddlewareResponseBody: true,
+		fontLoaders: [{ loader: '@next/font/google' }],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
